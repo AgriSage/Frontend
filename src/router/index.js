f6465bca3a291e165e21aff21ba0f22b161e509d
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-30 22:59:44 -0500

    fix(routes): deleted duplicate component

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../public/pages/Home/home.compoment.vue";
 import notFoundComponent from "../public/pages/NotFound/notFound.component.vue";
-import HomeCompoment from "../public/pages/Home/home.compoment.vue";
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes: [
-		{ path: "/home", component: HomeCompoment, meta: { title: "Home" } },
+		{ path: "/home", component: Home, meta: { title: "Home" } },
 		{ path: "/", redirect: "/home", meta: { title: "Home" } },
 		{ path: '/:pathMatch(.*)*', component: notFoundComponent},
 	],
