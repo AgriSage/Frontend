9cae52ebc12e1fb7d85cc12ae776ad9bbc3c993e
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-04-30 22:50:39 -0500

    refactor(home): home component to math router

---
+++ src/public/pages/Home/home.compoment.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+<template>
+	<h1>Home page of AgriSage</h1>
+</template>
+
+<style>
+
+</style>
--- src/public/pages/home.compoment.vue
+++
@@ -1,3 +0,0 @@
-<template>
-	<h1>Home page of AgriSage</h1>
-</template>
