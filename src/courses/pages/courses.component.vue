caaf7956b3ad02e631969057e010675600607505
DIEGO <u202219266@upc.edu.pe>
2024-05-03 04:34:02 -0500

    feat: Added Profile and Shop

---
+++ src/courses/pages/courses.component.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
+<template>
+  <h1>Courses</h1>
+</template>
+
+<style>
+h1 {
+  color: #000; /* Color negro */
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+//PrimeVue
+import PrimeVue from "primevue/config";
+// PrimeVue Material Design Theme
+import "primevue/resources/themes/mdc-light-indigo/theme.css";
+// PrimeVue Services
+import ToastService from "primevue/toastservice";
+import ConfirmationService from "primevue/confirmationservice";
+import DialogService from "primevue/dialogservice";
+
+// Router
+import router from "./router";
+
+createApp(App)
+	.use(router)
+	.use(PrimeVue, { ripple: true })
+	.use(DialogService)
+	.use(ConfirmationService)
+	.use(ToastService)
+	.mount("#app");
