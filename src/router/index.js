5de949eb28a86e52d67e2e5ccbfbb7bbc2735c71
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-03 02:17:37 -0500

    feat(router): added cart to routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,7 @@ import Home from "../public/pages/Home/home.compoment.vue";
 import notFoundComponent from "../public/pages/NotFound/notFound.component.vue";
 import Store from "../shop/pages/shop.component.vue";
 import Courses from "../courses/pages/courses.component.vue";
-
-
+import cartPageComponent from "../cart/pages/cart-page.component.vue";
 
 const router = createRouter({
 	history: createWebHistory(),
@@ -13,7 +12,8 @@ const router = createRouter({
 		{ path: "/", redirect: "/home", meta: { title: "Home" } },
 		{ path: "/store", component: Store, meta: { title: "Tienda" } },
 		{ path: "/courses", component: Courses, meta: { title: "Cursos" } },
-		{ path: '/:pathMatch(.*)*', component: notFoundComponent},
+		{ path: "/cart", component: cartPageComponent, meta: { title: "Carrito" } },
+		{ path: "/:pathMatch(.*)*", component: notFoundComponent },
 	],
 });
 
